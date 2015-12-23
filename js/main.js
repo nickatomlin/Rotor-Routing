c672ef7d2522b0bb0426c6aabddf7181655d6caa
Nicholas Tomlin <Nicholas_Tomlin@brown.edu>
2015-12-23 14:22:51 -0600

    Added Canvas

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-document.write('<h3>this is some test JS output</h3>');
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+
+// Create gradient
+var grd = ctx.createLinearGradient(0,0,500,0);
+grd.addColorStop(0,"red");
+grd.addColorStop(1,"white");
+
+// Fill with gradient
+ctx.fillStyle = grd;
+ctx.fillRect(10,10,500,500);
