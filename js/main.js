a63a6b1de4cbfb136c6b8df2e76df4a6b33b914c
Nicholas Tomlin <Nicholas_Tomlin@brown.edu>
2015-12-27 02:52:55 -0600

    Updated JS

--- js/main.js
+++ js/main.js
@@ -1,62 +1,72 @@
 var canvas;
 var ctx;
-var dx = 1;
-var dy = 2;
-var bar=new Bar(400,500);
-var circle=new Circle(400,30,10);
-var dxBar=6;
 var timer;
-var barImg;
-function Bar(x,y){
-    this.x=x;
-    this.y=y;
-}
-function Circle(x,y,r){
-    this.x=x;
-    this.y=y;
-    this.r=r;
-}
-function drawBall(c) {
-    ctx.beginPath();
-    ctx.arc(c.x, c.y, c.r, 0, Math.PI*2, true);
-    ctx.fill();
-}
-function doKeyDown(e){
-    if(e.keyCode==37){
-        if(bar.x-dxBar>0)
-            bar.x-=dxBar;
-    }
-    else if(e.keyCode==39){
-        if(bar.x+dxBar<canvas.width)
-            bar.x+=dxBar;
+var matrix = [];
+for(var i = 0; i < 50; i++) {
+    matrix[i] = [];
+    for(var j = 0; j < 50; j++) {
+        matrix[i][j] = 0;
     }
 }
+var locations = [];
+
 function init() {
-    window.addEventListener("keydown",doKeyDown,false);
-    barImg=document.getElementById("bar");
     canvas = document.getElementById("canvas");
     ctx = canvas.getContext("2d");
-    timer=setInterval(draw, 10);
+    timer = setInterval(update, 50);
     return timer;
 }
-function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "#FAF7F8";
-    ctx.fillRect(0,0,canvas.width,canvas.height);
-    ctx.fillStyle = "#003300";
-    drawBall(circle);
-    if (circle.x +dx > canvas.width || circle.x +dx < 0)
-        dx=-dx;
-    if(circle.y+dy>bar.y && circle.x>bar.x && circle.x<bar.x+barImg.width)
-        dy=-dy;
-    if (circle.y +dy > canvas.height || circle.y +dy < 0)
-        dy=-dy;
-    circle.x += dx;
-    circle.y += dy;
-    ctx.drawImage(barImg,bar.x,bar.y);
-    if(circle.y>bar.y){
-        clearTimeout(timer);
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        alert("Game Over");
+function update() {
+  for (var d = 0; d < 10; d++) {
+    if (d == 0) {
+      locations.push([24,24]);
     }
+    else {
+      var count = locations.length;
+      var new_locations = [];
+
+      for(var i = 0; i < count; i++) {
+        var item = locations[i];
+        var x = item[0];
+        var y = item[1];
+        if (matrix[x][y] == 0) {
+          if (y > 0) {
+            var new_item = [x, y - 1];
+            new_locations.push(new_item);
+            matrix[x][y] = 1;
+            ctx.fillStyle = "blue";
+            ctx.fillRect(x*10, y*10, 10, 10);
+          }
+        }
+        else if (matrix[x][y] == 1) {
+          if (x > 0) {
+            var new_item = [x - 1, y];
+            new_locations.push(new_item);
+            matrix[x][y] = 2;
+            ctx.fillStyle = "red";
+            ctx.fillRect(x*10, y*10, 10, 10);
+          }
+        }
+        else if (matrix[x][y] == 2) {
+          if (y < 49) {
+            var new_item = [x, y + 1];
+            new_locations.push(new_item);
+            matrix[x][y] = 3;
+            ctx.fillStyle = "green";
+            ctx.fillRect(x*10, y*10, 10, 10);
+          }
+        }
+        else if (matrix[x][y] == 3) {
+          if (x < 49) {
+            var new_item = [x + 1, y];
+            new_locations.push(new_item);
+            matrix[x][y] = 0;
+            ctx.fillStyle = "yellow";
+            ctx.fillRect(x*10, y*10, 10, 10);
+          }
+        }
+      }
+      locations = new_locations;
+    }
+  }
 }
