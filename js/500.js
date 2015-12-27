b4bfc385c6c57a7a6c65a9703a95b991381b70ab
Nicholas Tomlin <Nicholas_Tomlin@brown.edu>
2015-12-27 13:09:01 -0600

    added comments

--- js/500.js
+++ js/500.js
@@ -1,6 +1,8 @@
 var canvas;
-var ctx;
+var ctx; // canvas object to which functions like fillRect are applied
 var timer;
+/* Defines a matrix with dimension 500 and fills it with zeroes. This matrix
+   is used to store the directions of arrows at each point on the grid. */
 var matrix = [];
 for(var i = 0; i < 500; i++) {
     matrix[i] = [];
@@ -8,15 +10,20 @@ for(var i = 0; i < 500; i++) {
         matrix[i][j] = 0;
     }
 }
+/* An array of locations of sand grains, represented by their lattice points.
+   Initialized as empty, but may look like [[241,1], [32,454]] after update has
+   been repeatedly called. */
 var locations = [];
 
 function init() {
     canvas = document.getElementById("canvas");
     ctx = canvas.getContext("2d");
-    timer = setInterval(update, 10);
+    timer = setInterval(update, 10); // calls update every 10 milliseconds
     return timer;
 }
 function update() {
+  /* Adds a new grain of sand every tenth iteration. In the future, we will
+     make this a selectable parameter for the website user. */
   for (var d = 0; d < 10; d++) {
     if (d == 0) {
       locations.push([249,249]);
@@ -24,19 +31,25 @@ function update() {
     else {
       var count = locations.length;
       var new_locations = [];
-
+      /* Iterates over every grain of sand in locations. Since the sand grains
+         will not stay in place, their new locations are stored in the
+         new_locations array. */
       for(var i = 0; i < count; i++) {
-        var item = locations[i];
-        var x = item[0];
-        var y = item[1];
+        var item = locations[i]; // the current grain of sand
+        var x = item[0]; // the x-coordinate of the sand grain
+        var y = item[1]; // the y-coordinate of the sand grain
+        /* A series of if statements checks the direction of the arrow at the
+           location of the current grain of sand. It fills the */
         if (matrix[x][y] == 0) {
+          /* These nested if statements remove any grains of sand that are about
+             to exit the matrix. */
           if (y > 0) {
             var new_item = [x, y - 1];
             new_locations.push(new_item);
           }
-          matrix[x][y] = 1;
-          ctx.fillStyle = "blue";
-          ctx.fillRect(x, y, 1, 1);
+          matrix[x][y] = 1; // changes arrow direction
+          ctx.fillStyle = "blue"; // sets fill color
+          ctx.fillRect(x, y, 1, 1); // visualization for arrow direction
         }
         else if (matrix[x][y] == 1) {
           if (x > 0) {
@@ -66,7 +79,7 @@ function update() {
           ctx.fillRect(x, y, 1, 1);
         }
       }
-      locations = new_locations;
+      locations = new_locations; // updates locations array
     }
   }
 }
